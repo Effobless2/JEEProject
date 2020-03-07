3285d5dcb07d03d4d2f8bc3aab895be11fce3fbf
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-03-07 15:12:32 +0100

    Project initialized

---
+++ src/main/java/com/esgi/group5/jeeproject/JeeprojectApplication.java
@@ -0,0 +1,13 @@
+package com.esgi.group5.jeeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JeeprojectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JeeprojectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/esgi/group5/jeeproject/JeeprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.esgi.group5.jeeproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JeeprojectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
