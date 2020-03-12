70f4550cdec65a02597e3b5a2239f0741d39a803
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-03-12 15:41:00 +0100

    Fixing User's Unit Test bug that all tests are valid

--- src/test/java/com/esgi/group5/jeeproject/services/users/AUserServiceTest.java
+++ src/test/java/com/esgi/group5/jeeproject/services/users/AUserServiceTest.java
@@ -5,12 +5,17 @@ import com.esgi.group5.jeeproject.repositories.contracts.IUserRepository;
 import com.esgi.group5.jeeproject.services.contracts.IUserService;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.Mockito.mock;
 
 public abstract class AUserServiceTest {
-    private String name = "test1";
+    private String name = "test";
     protected IUserService userService;
 
     protected IUserRepository userRepository;
@@ -21,15 +26,28 @@ public abstract class AUserServiceTest {
 
     @Test
     void should_be_empty_after_init(){
+        given(userRepository.get()).willReturn(new ArrayList<>());
         assertThat(userService.get().isEmpty());
     }
 
     @Test
     void should_add_new_user_and_returns_new_user_id(){
-        assertThat(userService.get().isEmpty());
-        userService.add(new User(name));
-        assertThat(userService.get().size() == 1);
-        User u = userService.get(0);
-        assertThat(u != null);
+        ArrayList<User> mockUsers = new ArrayList<>();
+        given(userRepository.get()).willReturn(mockUsers);
+        for(int i = 0; i < 10; i++){
+            String curName = name + i;
+            User test = new User(curName);
+            mockUsers.add(test);
+
+            given(userRepository.add(test)).willReturn(i);
+            int id = userService.add(test);
+            assertEquals(i, id);
+            assertEquals(i + 1, userService.get().size());
+
+            given(userRepository.get(id)).willReturn(test);
+            User u = userService.get(id);
+            assertNotNull(u);
+            assertEquals(curName, u.getName());
+        }
     }
 }
