a700a4fef5ef9022840529d19b55cb36bc6bc29d
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-03-22 13:03:04 +0100

    End 2 End test realized

--- src/main/java/com/esgi/group5/jeeproject/controllers/UserController.java
+++ src/main/java/com/esgi/group5/jeeproject/controllers/UserController.java
@@ -3,8 +3,12 @@ package com.esgi.group5.jeeproject.controllers;
 import com.esgi.group5.jeeproject.models.User;
 import com.esgi.group5.jeeproject.services.contracts.IUserService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.validation.Valid;
 import java.util.List;
 
 @RestController
@@ -24,7 +28,11 @@ public class UserController {
     }
 
     @PostMapping
-    public int post(@RequestBody User user){
-        return userService.add(user);
+    public ResponseEntity<?> post(HttpServletRequest request, @RequestBody @Valid User user){
+        int id = userService.add(user);
+
+        return ResponseEntity
+                .status(HttpStatus.CREATED)
+                .body(id);
     }
 }
