626eaae2a919a2a32c272d7bbf7db101dd49661d
Lenoir Romain <romain-L58@hotmail.fr>
2020-04-12 12:59:51 +0200

    secure get Beers route

--- src/main/java/com/esgi/group5/jeeproject/controllers/BeerController.java
+++ src/main/java/com/esgi/group5/jeeproject/controllers/BeerController.java
@@ -1,8 +1,12 @@
 package com.esgi.group5.jeeproject.controllers;
 
 import com.esgi.group5.jeeproject.models.Beer;
+import com.esgi.group5.jeeproject.models.User;
+import com.esgi.group5.jeeproject.security.JwtUtilService;
 import com.esgi.group5.jeeproject.services.contracts.IBeerService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/beers")
@@ -17,9 +22,17 @@ import java.util.List;
 public class BeerController {
     private final IBeerService service;
 
+    @Autowired
+    JwtUtilService jwtUtilService;
+
     @GetMapping
-    public List<Beer> get(){
-        return service.get();
+    public ResponseEntity<?> get(@RequestHeader Map<String, String> headers){
+        if(jwtUtilService.validateToken(headers.get("authorization"))){
+            List<Beer> beers = service.get();
+            return ResponseEntity.ok().body(beers);
+        }else{
+            return ResponseEntity.status(403).body("access denied");
+        }
     }
 
     @GetMapping("/{beerId}")
