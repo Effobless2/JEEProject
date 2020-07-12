c8025ef4c448d0f54656c573715ec6aab3fea330
Maxime Deboffle <maxime.deboffle@hotmail.com>
2020-07-12 21:22:38 +0200

    main root 200 ok nice sa mère

---
+++ src/main/java/com/esgi/group5/jeeproject/infrastructure/web/controllers/TestController.java
@@ -0,0 +1,22 @@
+package com.esgi.group5.jeeproject.infrastructure.web.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+@RestController
+@RequestMapping("/")
+@CrossOrigin(origins = "*")
+public class TestController {
+    @GetMapping
+    public ResponseEntity<?> get(){
+
+        return ResponseEntity
+                .status(HttpStatus.OK)
+                .body("Hello World");
+    }
+}
